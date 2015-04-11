647449f9a73fe1ec9d3be56db3bef4fcbaa330e1
kachingy123 <bensusman@gmail.com>
2015-04-11 16:15:40 +0100

    Improved tokenizer

--- test/test_main.py
+++ test/test_main.py
@@ -3,22 +3,25 @@
 import nltk
 import json
 from nltk.tag.brill import Pos
+import re
 
 
 with open('london-marathon-2015-03-18') as f:
     for line in f:
         tweet = json.loads(line)
-        
         # Tokenize Text
         text = tweet['text'].replace('\n', ' ')
         text = text.replace('?', ' ?')
+        text = text.replace('!', ' !')
         text = text.replace('...', ' ')
+        text = re.sub('\\s\(', ' ( ', text)
+        text = re.sub('([A-Za-z0-9])\)', '\g<1> ) ', text)
         tokens = text.split(' ')
         
         # Get POS Tags
         posTags = nltk.pos_tag(tokens)
         
-        print(posTags)
+        #print(posTags)
         
         '''
         tokenization = nltk.word_tokenize(tweet['text']) 
